1c40929f407e40c120b1e9b9528c003efd1fa613
Tiago Marcial <tiko127@hotmail.com>
2025-10-01 23:11:20 -0300

    feat: estrutura XML da bottom navigation na MainActivity

---
+++ app/src/main/java/dev/tiagomarcial/instagram/main/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.tiagomarcial.instagram.main.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dev.tiagomarcial.instagram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
