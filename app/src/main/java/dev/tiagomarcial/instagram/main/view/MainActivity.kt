d294d21243258284097c1ec59205285c1e5f7eda
Tiago Marcial <tiko127@hotmail.com>
2025-10-06 21:39:47 -0300

    feat: finalização da RecyclerView da tela de perfil

--- app/src/main/java/dev/tiagomarcial/instagram/main/view/MainActivity.kt
+++ app/src/main/java/dev/tiagomarcial/instagram/main/view/MainActivity.kt
@@ -1,8 +1,10 @@
 package dev.tiagomarcial.instagram.main.view
 
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import dev.tiagomarcial.instagram.R
@@ -13,5 +15,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        window.insetsController?.setSystemBarsAppearance(
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+        )
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
     }
 }
